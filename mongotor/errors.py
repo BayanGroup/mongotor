85f9428576be182a9efa6acf0c350ce6a76e66df
Taha Jahangir <mtjahangir@gmail.com>
2014-06-05 09:25:01 +0430

    Use Exceptions as errors base class (instead of StandardError)

--- mongotor/errors.py
+++ mongotor/errors.py
@@ -16,7 +16,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-class Error(StandardError):
+class Error(Exception):
     """Base class for all mongotor exceptions.
 
     """
